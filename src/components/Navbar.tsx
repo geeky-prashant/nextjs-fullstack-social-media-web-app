e05cd41acc60f96362f86d67caa3f2694a53499f
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-09-27 17:17:48 +0530

    Configured Clerk authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Link from "next/link"
 import Image from "next/image"
 import MobileMenu from "./MobileMenu"
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
 
 const Navbar = () => {
   return (
@@ -49,6 +53,47 @@ const Navbar = () => {
 
       {/* RIGHT  */}
       <div className="flex items-center gap-4">
+        <ClerkLoading>
+          <div
+            className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image
+                src="/people.png"
+                alt="People"
+                width={20}
+                height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/messages.png"
+                alt="Messages"
+                width={20}
+                height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/notifications.png"
+                alt="Notifications"
+                width={20}
+                height={20} />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="cursor-pointer flex items-center gap-2 text-base">
+              <Image
+                src="/login.png"
+                alt="Login"
+                width={20}
+                height={20} />
+              <Link href="/">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
