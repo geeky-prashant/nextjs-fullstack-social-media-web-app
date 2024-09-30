f9c10f7408c347707b0dcd053653db4541a25f0d
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-09-30 23:27:58 +0530

    Completed Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -39,7 +39,50 @@ const Post = () => {
       </div>
 
       {/* INTERACTION  */}
-      <div className=""></div>
+      <div className="flex items-center justify-between text-sm mt-4">
+        <div className="flex gap-6">
+          {/* LIKES  */}
+          <div className="flex items-center gap-4 bg-slate-100 p-3 rounded-xl">
+            <Image
+              src="/like.png"
+              alt="Like"
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-400">|</span>
+            <span className="text-gray-500">1.2K+<span className="hidden md:hidden lg:inline"> Likes</span></span>
+          </div>
+
+          {/* COMMENTS  */}
+          <div className="flex items-center gap-4 bg-slate-100 p-3 rounded-xl">
+            <Image
+              src="/comment.png"
+              alt="Comment"
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-400">|</span>
+            <span className="text-gray-500">500+<span className="hidden md:hidden lg:inline"> Comments</span></span>
+          </div>
+        </div>
+
+        <div className="">
+          <div className="flex items-center gap-4 bg-slate-100 p-3 rounded-xl">
+            {/* SHARE  */}
+            <Image
+              src="/share.png"
+              alt="Share"
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-400">|</span>
+            <span className="text-gray-500">200+<span className="hidden md:hidden lg:inline"> Shares</span></span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
